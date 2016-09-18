9cd01cbc914354e23f6a81ac0ce55a93e99471b3
Victor Szoltysek <victorszoltysek@Victors-MacBook-Pro.local>
2016-09-18 07:39:29 -0400

    Initial commit -- simple PhantomJS usage

---
+++ src/main/java/Example.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+ 
+public class Example {
+
+    public static void main(String[] args) {
+        WebDriver driver = new PhantomJSDriver();
+        driver.get("http://www.google.com");
+
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Cheese!");
+        element.submit();
+
+        System.out.println("Page title is: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
